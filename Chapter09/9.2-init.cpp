ffa123bcb4d904a3171add1696530df0c21ab0bb
ArthurWangSuper <cp3wt10@163.com>
2018-12-12 20:10:56 +0800

    Update 9.2-init.cpp

--- Chapter09/9.2-init.cpp
+++ Chapter09/9.2-init.cpp
@@ -4,6 +4,7 @@
 #include <list>
 
 using namespace std;
+
 int printvec(vector<int>& ivec)
 {
 	vector<int>::iterator it = ivec.begin();
@@ -26,11 +27,19 @@ int main(int argc,char**argv)
 	vector<int> ivec5(ivec4);
 	vector<int> ivec6(ilist.begin(),ilist.end());
 	
+	vector<double> dvec1(ilist.begin(),ilist.end());
+	vector<double> dvec2{1.1,2.2,3.3,5.4,4.5,1.6,7.7,8.8,9.9};
+	vector<int> ivec7(dvec2.begin(),dvec2.end());
+
 	printvec(ivec2);	
 	printvec(ivec3);	
 	printvec(ivec4);	
 	printvec(ivec5);	
 	printvec(ivec6);
-	
+	printvec(ivec7);
+
+	for(auto element:dvec1)
+		cout<<element<<" ";
+
 	return 0;
 }
